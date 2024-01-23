1c3d4643ed416f319cafcf53d635cfb89aae4c86
RuanGeldenhuys <ruangeldenhuys.work@gmail.com>
2024-01-23 17:43:03 +0200

    Cleaned up code + Kable testing

--- code/renamingDCC.R
+++ code/renamingDCC.R
@@ -13,4 +13,16 @@ renamingdcc <- function(ReturnSeries, DCC.TV.Cor) {
             nam[(i + (j-1)*(ncolrtn))] <- paste(xx[[j]][1], xx[[i]][1], sep="_")
         }
     }
+
+    colnames(DCC.TV.Cor) <- nam
+
+    # So to plot all the time-varying correlations wrt SBK:
+    # First append the date column that has (again) been removed...
+    DCC.TV.Cor <-
+        data.frame( cbind( date = index(ReturnSeries), DCC.TV.Cor)) %>% # Add date column which dropped away...
+        mutate(date = as.Date(date)) %>%  tbl_df()
+
+    DCC.TV.Cor <- DCC.TV.Cor %>% gather(Pairs, Rho, -date)
+
+    DCC.TV.Cor
 }
