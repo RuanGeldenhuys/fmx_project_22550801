1916090934ce90931a02825c4872e4d8a4c014f4
RuanGeldenhuys <ruangeldenhuys.work@gmail.com>
2024-01-25 10:51:14 +0200

    Setting up and testing Texevier

---
+++ Texevier/Template/code/returns_plotter.R
@@ -0,0 +1,39 @@
+returns_plotter <- function(df, names){
+    datalist <- list()
+    plotlist <- list()
+
+    for (i in c(2:4)) {
+
+        datalist[[i-1]] <- df %>%
+            select(c(1, i)) %>%
+            rename(Returns = names(.)[2]) %>%
+            mutate(`Absolute Returns` = abs(Returns)) %>%
+            mutate(`Squared Returns` = Returns^2) %>%
+            gather(key = Type, value = Returns, -date)
+
+    }
+
+    names(datalist) <- names
+
+    for (j in c(1:3)) {
+
+        datalist[[j]]$Type <- factor(datalist[[j]]$Type,
+                                     levels = c("Returns", "Absolute Returns",
+                                                "Squared Returns"))
+
+
+        g1 <- ggplot(datalist[[j]]) +
+            geom_line(aes(x = date, y = Returns, colour = Type))+
+            geom_hline(yintercept = 0)+
+            ggtitle(names[j])+
+            facet_wrap(~Type, nrow = 3, scales = "free")+
+            guides(alpha = "none", colour = "none") +
+            fmxdat::theme_fmx()
+
+        plotlist[[j]] <- finplot(g1)
+    }
+
+    names(plotlist) <- names
+
+    return(plotlist)
+}
